0b0cfe757d47d13055b09a8f3c84327fb62182aa
Dawid930 <davidnyikes@gmail.com>
2022-02-15 19:56:54 +0100

    practice

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,49 @@
-
+/* 
 const fruits = ['apple', 'orange', 'pears'];
 
-fruits[3] = 'grapes';
+fruits[3] = 'grapes'; //a vegere berakja, de a lenti jobb
 
 
-fruits.push('mangos')
+fruits.push('mangos') // a vegere berakja
 
-fruits.unshift('strawberries')
+fruits.unshift('strawberries')  // az elejere berakja
 
+fruits.pop() // kiveszi az utolsot
+
+console.log(Array.isArray(fruits));  //check if fruits really array or not
+
+console.log(fruits.indexOf('orange')); // show the index nr of a particular item
 
 console.log(fruits);
+
+
+
+const x = 4;
+const y = 10;
+
+if(x > 5 || y > 10) {   // the sign ||  means OR; && means AND;   ?  means THEN
+    console.log();
+}
+
+ */
+const x = 10;
+
+const color = x > 10 ? 'red' : 'blue';   // ?  means THEN;  : means ELSE
+
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT red or blue');
+        break;
+}
+
+
+
+
+
 
